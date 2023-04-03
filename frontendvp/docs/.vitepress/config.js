1fc1c1238403f8c864e89b1a0f63ed4104d22886
zhiyiyi <1187303661@qq.com>
2023-04-03 23:22:11 +0800

    update

--- frontendvp/docs/.vitepress/config.js
+++ frontendvp/docs/.vitepress/config.js
@@ -1,3 +1,7 @@
+import { BasicMenu } from "./menu/basic-menu";
+import { InterviewMenu } from "./menu/interview-menu";
+import { MainNav } from "./menu/main-nav";
+
 export default {
 	title: "ZerFe",
 	themeConfig: {
@@ -9,36 +13,7 @@ export default {
 		logo: "/icons/index.svg",
 		siteTitle: "ZerFrontend",
 		lastUpdated: true,
-		nav: [
-			{
-				text: "首页",
-				link: "/",
-			},
-			{
-				text: "快速开始",
-				items: [
-					{
-						text: "HTML",
-						link: "/basic/html/h1",
-					},
-					{
-						text: "CSS",
-						link: "/basic/css/c1-svg",
-					},
-					{
-						text: "Javascript",
-						link: "/basic/js/j1",
-					},
-				],
-				activeMatch: "/basic/",
-			},
-			{
-				text: "前端进阶",
-				link: "/deep/dep",
-				activeMatch: "/deep/",
-			},
-			{ text: "项目分析", link: "/project/prj", activeMatch: "/project/" },
-		],
+		nav: MainNav, // 导航抽离
 		socialLinks: [
 			{ icon: "github", link: "https://github.com/zhiyiyi" },
 			{ icon: "discord", link: "https://discord.gg/Qs4Mtr23QG" },
@@ -56,33 +31,11 @@ export default {
 			prev: "上一篇文章",
 			next: "下一篇文章",
 		},
+
+		// 侧边栏抽离
 		sidebar: {
-			"/basic/": [
-				{
-					text: "HTML",
-					collapsed: true,
-					items: [
-						{ text: "HTML快速入门", link: "/basic/html/h1" },
-						{
-							text: "HTTP协议及详解",
-							link: "/basic/html/h2",
-						},
-					],
-				},
-				{
-					text: "CSS",
-					collapsed: true,
-					items: [
-						{ text: "SVG详解", link: "/basic/css/c1-svg" },
-						{ text: "WebGL原理与基础案例", link: "/basic/css/c2-webgl" },
-					],
-				},
-				{
-					text: "Javascript",
-					collapsed: true,
-					items: [{ text: "基础语法速通", link: "/basic/js/j1" }],
-				},
-			],
+			"/basic/": BasicMenu,
+			"/interview/": InterviewMenu,
 		},
 	},
 	cleanUrls: true,
---
+++ frontendvp/docs/.vitepress/menu/main-nav.js
@@ -0,0 +1,31 @@
+export const MainNav = [
+	{
+		text: "首页",
+		link: "/",
+	},
+	{
+		text: "快速开始",
+		items: [
+			{
+				text: "HTML",
+				link: "/basic/html/h1",
+			},
+			{
+				text: "CSS",
+				link: "/basic/css/c1-svg",
+			},
+			{
+				text: "Javascript",
+				link: "/basic/js/j1",
+			},
+		],
+		activeMatch: "/basic/",
+	},
+	{
+		text: "前端进阶",
+		link: "/deep/dep",
+		activeMatch: "/deep/",
+	},
+	{ text: "项目分析", link: "/project/prj", activeMatch: "/project/" },
+	{ text: "面试", link: "/interview/index", activeMatch: "/interview/" },
+];
