5137139e613b0adaa32f5ed14578ea7862d1fbe3
zhiyiyi <1187303661@qq.com>
2023-09-05 13:47:36 +0800

    update

--- javarush/docs/.vitepress/menu/java-menu.js
+++ javarush/docs/.vitepress/menu/java-menu.js
@@ -1,8 +1,12 @@
 export const JavaMenu = [
 	{
-		text: "Java基础复习",
+		text: "Java基础到进阶",
 		collapsed: true,
-		items: [{ text: "推荐面经网站", link: "/java/basic/j1" }],
+		items: [
+			{ text: "推荐面经网站", link: "/java/basic/j1" },
+			{ text: "Java基础", link: "/java/basic/j2" },
+			{ text: "Java高级特性", link: "/java/basic/j3" },
+		],
 	},
 	{
 		text: "Java8实战",
